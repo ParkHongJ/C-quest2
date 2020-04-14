580656251e87c08dd074edbe21c21fb1a0682e07
ParkHongJ <qkrghdwns98@naver.com>
2020-04-14 23:13:03 +0900

    C# 과제
    7.0이전버전으로 object switch문 만들기

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+
+public class Test
+{
+    public static void Main()
+    {
+        object obj = 1230;
+        int b = 0;
+        if (obj.GetType().Name == "Int32")
+        {
+            b = 1;
+        }
+        else if(obj.GetType().Name == "Single")
+        {
+            b = 2;
+        }
+        switch(b)
+        {
+            case 1:
+                Console.WriteLine("정수입니다");
+                break;
+            case 2:
+                Console.WriteLine("실수입니다.");
+                break;
+            default:
+                break;
+
+        }
+        //obj.GetType()
+    }
+}
